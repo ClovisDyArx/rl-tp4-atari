84da6ea7eb5afc12c3e3641ac319db868b43157a
Clovis Lechien <clovis.lechien@epita.fr>
2024-10-26 11:41:40 +0200

    feat(mem): added the memory handling class

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# rl-tp4-atari
 # ***TODO***: write a cool description.
---
+++ src/replay_buffer.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+# Handling the replay buffer, keeping track of the state, action, reward, and new state.
+class ReplayBuffer(object):
+    def __init__(self, max_size, input_shape):
+        self.mem_size = max_size
+        self.mem_cntr = 0
+
+        self.state_memory = np.zeros((self.mem_size, *input_shape), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_shape), dtype=np.float32)
+        self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
+        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.uint8)
+
+    # Storing the state, action, reward, new state, and done flag.
+    def store_transition(self, state, action, reward, new_state, done):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.new_state_memory[index] = new_state
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
+
+        self.mem_cntr += 1
+
+    # Sampling a batch of experiences from the replay buffer.
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.mem_cntr, self.mem_size)
+
+        batch = np.random.choice(max_mem, batch_size, replace=False)  # not sampling the same experience twice
+
+        states = self.state_memory[batch]
+        new_states = self.new_state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        dones = self.terminal_memory[batch]
+
+        return states, actions, rewards, new_states, dones

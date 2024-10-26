be8287bc516b92e885f01a5ac9694688fe755044
Clovis Lechien <clovis.lechien@epita.fr>
2024-10-26 13:33:39 +0200

    feat(torch): whole architecture done. issue with gym[atari]. will need to fix this asap. need to implement keras version first just to be sure its good. pytorch version has its foundations.

---
+++ src/pytorch/main_dqn.py
@@ -0,0 +1,60 @@
+import numpy as np
+from agent import Agent
+from src.utils import make_env, plot_learning_curve
+
+
+if __name__ == '__main__':
+    env = make_env('PongNoFrameskip-v4')
+    num_games = 250
+    load_checkpoint = False
+    best_score = -21
+    agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.0001,
+                  input_dims=(4, 80, 80),
+                  n_actions=6, mem_size=25000, epsilon_min=0.02,
+                  batch_size=32, replace=1000, epsilon_dec=1e-5)
+
+    if load_checkpoint:
+        agent.load_models()
+
+    fname = 'PongNoFrameskip-v4.png'
+
+    scores, eps_history = [], []
+    n_steps = 0
+
+    for i in range(num_games):
+        done = False
+        observation = env.reset()
+        score = 0
+
+        while not done:
+            action = agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            n_steps += 1
+            score += reward
+            if not load_checkpoint:
+                agent.store_transition(observation, action, reward, observation_, int(done))
+                agent.learn()
+            else:
+                env.render()
+            observation = observation_
+
+        scores.append(score)
+        avg_score = np.mean(scores[-100:])
+
+        print(f"episode {i},"
+              f"score: {score},"
+              f"average score: {avg_score},"
+              f"best score: {best_score},"
+              f"epsilon: {agent.epsilon},"
+              f"steps: {n_steps}"
+              "\n\n")
+
+        if avg_score > best_score:
+            agent.save_models()
+            print(f"New best score: {avg_score},"
+                  f"better than score: {best_score}"
+                  f"\n")
+            best_score = avg_score
+
+        x = [i+1 for i in range(num_games)]
+        plot_learning_curve(x, scores, eps_history, fname)
--- src/utils.py
+++ src/utils.py
@@ -75,3 +75,30 @@ def make_env(env_name):
     env = MoveImgChannel(env)
     env = BufferWrapper(env, 4)
     return ScaleFrame(env)
+
+
+def plot_learning_curve(x, scores, eps_history, fname):
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(111, label='1')
+    ax2 = fig.add_subplot(111, label='2', frame_on=False)
+
+    ax.plot(x, eps_history, color='C0')
+    ax.set_xlabel('Game', color='C0')
+    ax.set_ylabel('Epsilon', color='C0')
+    ax.tick_params(axis='x', colors='C0')
+    ax.tick_params(axis='y', colors='C0')
+
+    N = len(scores)
+    run_avg = np.empty(N)
+    for t in range(N):
+        run_avg[t] = np.mean(scores[max(0, t-10):(t+1)])
+
+    ax2.scatter(x, run_avg, color='C1')
+    ax2.axes.get_xaxis().set_visible(False)
+    ax2.yaxis.tick_right()
+    ax2.set_ylabel('Score', color='C1')
+    ax2.yaxis.set_label_position('right')
+    ax2.tick_params(axis='y', colors='C1')
+
+    plt.savefig(fname)
